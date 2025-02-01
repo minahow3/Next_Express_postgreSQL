5a10fe178fb864af62756a4a53017c50aeca65f7
rena <68492530+minahow3@users.noreply.github.com>
2025-02-01 14:27:38 +0000

    eslintの設定を修正

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -11,6 +11,17 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn", // 🚨 エラーではなく警告に変更
+        { 
+          "argsIgnorePattern": "^_", // `_` で始まる未使用の引数は許可
+          "varsIgnorePattern": "^(__webpack_require__|exports|__unused_webpack_module)$" // Webpack 系変数は無視
+        }
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
